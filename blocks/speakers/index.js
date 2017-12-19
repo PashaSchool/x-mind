4ec749a26ba441e473f889e19674bcbf51d776ed
Pasha School <pasha.school888@gmail.com>
2017-12-19 17:48:44 +0200

    add timer / googlemap sections

--- blocks/speakers/index.js
+++ blocks/speakers/index.js
@@ -1,6 +1,5 @@
 $(function() {
 
-
 	var owl = $('.owl-carousel.speakers-carusel');
 	var owlSponsor = $('.owl-carousel.sponsor-carusel');
 	var owlPartners = $('.owl-carousel.partners-carusel');
@@ -82,3 +81,64 @@ $(function() {
 
 
 })
+
+function initMap() {
+		var option = {
+			center: {lat: 49.85, lng: 24.0166666667},
+			scrollwheel: false,
+			zoom: 18,
+			scaleControl: false,
+			zoomControl: false,
+			streetViewControl: false,
+			styles:[
+			{
+				"featureType": "all",
+				"stylers": [
+				{
+					"saturation": 0
+				},
+				{
+					"hue": "#e7ecf0"
+				}
+				]
+			},
+			{
+				"featureType": "road",
+				"stylers": [
+				{
+					"saturation": -70
+				}
+				]
+			},
+			{
+				"featureType": "transit",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "poi",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "water",
+				"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"saturation": -60
+				}
+				]
+			}
+			]
+		};
+
+	var map = new google.maps.Map(document.getElementById('map'), option);
+  }
